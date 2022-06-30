b2cc19ea6e33e95da87f7f1484bf70339f24fe33
Aziz Sobirov <azizsobirovdev@gmail.com>
2022-06-30 13:17:16 +0500

    33

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 // Импорт Telegraf и Markup
-const {
-  Telegraf,
-  Markup
-} = require('telegraf')
+const { Telegraf, Markup } = require("telegraf");
 // Импорт dotenv для защиты API токена
-require('dotenv').config()
+require("dotenv").config();
 // Импорт нашего модуля с константами
-const my_const = require('./const')
+const my_const = require("./const");
 // Инициализация бота с помощью Telegraf
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf("5537312113:AAHBC0M73um0Ia9bsQYtsXOIUFVxl-4edl0");
 
 // Обработка команды /start
-bot.start((ctx) => ctx.reply(`Привет ${ctx.message.from.first_name ? ctx.message.from.first_name : 'незнакомец'}!`))
+bot.start((ctx) =>
+  ctx.reply(
+    `Привет ${
+      ctx.message.from.first_name ? ctx.message.from.first_name : "незнакомец"
+    }!`
+  )
+);
 // Обработка команды /help
-bot.help((ctx) => ctx.reply(my_const.commands))
+bot.help((ctx) => ctx.reply(my_const.commands));
 // Обработка команды /course
-bot.command('course', async (ctx) => {
+bot.command("course", async (ctx) => {
   try {
-    await ctx.replyWithHTML('<b>Курсы</b>', Markup.inlineKeyboard(
-      [
-        [Markup.button.callback('Редакторы', 'btn_1'), Markup.button.callback('Обзоры', 'btn_2'), Markup.button.callback('JS', 'btn_3')]
-      ]
-    ))
+    await ctx.replyWithHTML(
+      "<b>Курсы</b>",
+      Markup.inlineKeyboard([
+        [
+          Markup.button.callback("Редакторы", "btn_1"),
+          Markup.button.callback("Обзоры", "btn_2"),
+          Markup.button.callback("JS", "btn_3"),
+        ],
+      ])
+    );
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
-})
+});
 /**
  * Функция для отправки сообщения ботом
  * @param {String} id_btn Идентификатор кнопки для обработки
@@ -36,28 +44,28 @@ bot.command('course', async (ctx) => {
 function addActionBot(id_btn, src_img, text, preview) {
   bot.action(id_btn, async (ctx) => {
     try {
-      await ctx.answerCbQuery()
+      await ctx.answerCbQuery();
       if (src_img !== false) {
         await ctx.replyWithPhoto({
-          source: src_img
-        })
+          source: src_img,
+        });
       }
       await ctx.replyWithHTML(text, {
-        disable_web_page_preview: preview
-      })
+        disable_web_page_preview: preview,
+      });
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
-  })
+  });
 }
 // Обработчик кнопок с помощью функции
-addActionBot('btn_1', './img/1.jpg', my_const.text1, true)
-addActionBot('btn_2', './img/2.jpg', my_const.text2, true)
-addActionBot('btn_3', false, my_const.text3, false)
+addActionBot("btn_1", "./img/1.jpg", my_const.text1, true);
+addActionBot("btn_2", "./img/2.jpg", my_const.text2, true);
+addActionBot("btn_3", false, my_const.text3, false);
 
 // Запустить бота
-bot.launch()
+bot.launch();
 
 // Включить плавную остановку
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
